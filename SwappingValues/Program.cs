ce2a14eb1816931047ced8aff1578c52294f2df5
Stepanov Vadim <vadgat777@mail.ru>
2022-07-01 13:38:11 +0700

    first commit

---
+++ SwappingValues/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwappingValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string surname;
+            string dataСhange;
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("\n Начальные неверные данные - имя: " + name + " и фамилия: " + surname);
+
+            dataСhange = name;
+            name = surname;
+            surname = dataСhange;
+
+            Console.WriteLine("\n Окончательные правильные данные - имя: " + name + " и фамилия: " + surname);
+        }
+    }
+}
